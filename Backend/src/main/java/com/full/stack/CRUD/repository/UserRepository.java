eaf4043ee6349c65ddcc892f014c18164996630c
WAUdeshinduminaweerakoon <weerako-se19024@stu.kln.ac.lk>
2023-08-04 21:33:51 +0530

    structure edited

--- Backend/src/main/java/com/full/stack/CRUD/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.full.stack.CRUD.repository;
-
-import com.full.stack.CRUD.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User,Long> {
-}
